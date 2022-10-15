4c9ff3c5e95b0988adab540a4a4df7d59b8d5a18
akmt14 <akshay.kamath.14@gmail.com>
2022-10-16 02:30:55 +0530

    single city file reference & formatting

--- src/weather-docker/dags/schedulepull.py
+++ src/weather-docker/dags/schedulepull.py
@@ -1,26 +1,20 @@
+from email import message
+
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.models import Variable
-
 from datetime import datetime
-import airflow_api
+import airflow_api as api
 
-with open("./metadata/00_local_cities.txt", 'r') as f:
+with open("./metadata/01_local_cities_name_fix.txt", 'r') as f:
     cities = f.read().splitlines()
 
-n = len(cities)//2
-cities_A, cities_B = ([cities[i:i + n] for i in range(0, len(cities), n)])
+with DAG("scheduled_api_pull_dag", start_date=datetime(2022,10,3), schedule_interval="@daily", catchup=False, max_active_runs=10) as dag:
 
-def api_caller(location_parent:str, location_child:str):
-    airflow_api.api_data_pull(location_parent, location_child)
-
-with DAG("scheduled_api_pull_dag", start_date=datetime(2022,10,3), schedule_interval="@daily", catchup=False) as dag:
-    
-    for city in cities_A[:2]:
+    for city in cities:
         city_report_pull=PythonOperator(
-        task_id='city_report_pull_{}'.format(city.replace(" ","")),
-        python_callable=api_caller,
-        op_args=["local",city]
-        )
+                task_id='city_report_pull_{}'.format(city.replace(" ","")),
+                python_callable=api.data_pull,
+                op_args=["local", city]
+                )
 
     city_report_pull
