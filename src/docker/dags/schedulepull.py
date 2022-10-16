d24842623e363492354cffb3048aaff7f50b4b2c
akmt14 <akshay.kamath.14@gmail.com>
2022-10-16 18:32:19 +0530

    postgresql connector attempt

--- src/docker/dags/schedulepull.py
+++ src/docker/dags/schedulepull.py
@@ -1,14 +1,43 @@
 from email import message
+from email.policy import default
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 import airflow_api as api
+from airflow.operators.postgres_operator import PostgresOperator
 
 with open("./metadata/01_local_cities_name_fix.txt", 'r') as f:
     cities = f.read().splitlines()
 
-with DAG("scheduled_api_pull_dag", start_date=datetime(2022,10,3), schedule_interval="@daily", catchup=False, max_active_runs=10) as dag:
+create_table_sql_query = """ 
+CREATE TABLE IF NOT EXISTS weather_daily_local_jsonb
+    (id SERIAL PRIMARY KEY,
+    queryCost INT NOT NULL,
+    latitude INT NOT NULL,
+    logitude INT NOT NULL,
+    resolvedAddress TEXT NOT NULL,
+    address TEXT NOT NULL,
+    timezone TEXT NOT NULL,
+    tzoffset INT NOT NULL,
+    days text[],
+    stations text []
+    );
+"""
+
+default_args = {
+    'owner' : 'airflow',
+    'start_date' : datetime(2022,10,3),
+    'schedule_interval' : "@daily",
+    'catchup' : False,
+    'max_active_runs' : 10,
+    'email' : ['akshay.kamath.14@gmail.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0    
+}
+
+with DAG("scheduled_api_pull_dag", default_args=default_args) as dag:
 
     for city in cities:
         city_report_pull=PythonOperator(
@@ -16,5 +45,11 @@ with DAG("scheduled_api_pull_dag", start_date=datetime(2022,10,3), schedule_inte
                 python_callable=api.data_pull,
                 op_args=["local", city]
                 )
+    create_table = PostgresOperator(
+        sql = create_table_sql_query,
+        task_id = "create_table_task",
+        postgres_conn_id = "postgres_local"
+        )
+        
 
     city_report_pull
