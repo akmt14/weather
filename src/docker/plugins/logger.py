456c7883659a907f1518c2282a3aabd522ec5c36
akmt14 <akshay.kamath.14@gmail.com>
2022-10-25 12:50:07 +0530

    space fix

--- src/docker/plugins/logger.py
+++ src/docker/plugins/logger.py
@@ -14,31 +14,25 @@ class log_record:
     """
 
     def __init__(self, _folder=None, _file=None, _error=None, _log=None):
-
         self._folder = _folder
         self._file = _file
         self._error = _error
         self._log = _log
 
     def success(self):
-
         curr_dt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         level_name = "INFO"
         self._log = '{}|{}|{}|{}\n'.format(curr_dt, level_name, self._folder, self._file)
-
         return self._log
 
     def fail(self):
-
         curr_dt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         level_name = "ERROR"
         error = self._error
         self._log = '{}|{}|{}|{}|{}\n'.format(curr_dt, level_name, error, self._folder, self._file)
-        
         return self._log
 
     @staticmethod
     def write_log(msg):
-
         with open (log_record._path + '02_daily_logs', mode='a', encoding='utf-8') as f:
             f.write(msg)
