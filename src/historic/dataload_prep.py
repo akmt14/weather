79f066a9607daf89d6d82061f48e5ed8d43fb192
akmt14 <akshay.kamath.14@gmail.com>
2022-10-27 16:18:16 +0530

    add location to historic files

---
+++ src/historic/dataload_prep.py
@@ -0,0 +1,19 @@
+import os
+import pandas as pd
+
+path='./data/01_historic/local/'
+
+columns = ['day', 'month', 'year', 'temp_f']
+
+for _file in os.listdir(path=path):
+    if _file.endswith('.txt'):
+        filename = _file.split('.')[0]+'.csv'
+        df = pd.read_fwf(path+_file, sep="\t", header=None, engine='python', encoding='utf-8')
+        df.columns = columns
+        df["state"] = os.path.basename(_file.split('.')[0].split("_")[0])
+        df["city"] = os.path.basename(" ".join(i for i in (_file.split('.')[0].split("_")[1:])))
+        df.to_csv(path+filename,sep=",",index=False)
+
+    
+
+
