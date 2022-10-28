6bc28b7723ac97fe5d4c7135a22f02e244f41ab2
akmt14 <akshay.kamath.14@gmail.com>
2022-10-28 12:48:53 +0530

    exclude headers from final file

--- src/historic/dataload_prep.py
+++ src/historic/dataload_prep.py
@@ -6,7 +6,7 @@ def transformer():
     """
     _summary_ : Add State & City name to file as a column.
     """
-    
+
     path='./data/01_historic/local/'
 
     columns = ['day', 'month', 'year', 'temp_f']
@@ -18,4 +18,8 @@ def transformer():
             df.columns = columns
             df["state"] = os.path.basename(_file.split('.')[0].split("_")[0])
             df["city"] = os.path.basename(" ".join(i for i in (_file.split('.')[0].split("_")[1:])))
-            df.to_csv(path+filename,sep=",",index=False)
+            df.to_csv(path+filename,sep=",",index=False, header=False)
+
+
+if __name__ == "__main__":
+    transformer()
