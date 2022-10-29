99364c362ce8dc1a72883d494981f70c5551319e
akmt14 <akshay.kamath.14@gmail.com>
2022-10-29 09:54:58 +0530

    path fix

--- src/docker/dags/sql/batch_upload.sh
+++ src/docker/dags/sql/batch_upload.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-hist_fpath="./data/02_daily/local/"
+hist_fpath="./data/01_historic/local/data_load/"
 
 for f in $hist_fpath*.csv;do
-
-    cat $f | psql -U postgres -d projects -c "COPY weather.daily_historic_local(day, month, year, temp_f, state, city) FROM STDIN DELIMITER ',' "
-
+    cat $f | psql -U postgres -d projects -c "COPY weather.daily_historic_local(month, day, year, temp_f, state, city) FROM STDIN DELIMITER ',' "
 done
