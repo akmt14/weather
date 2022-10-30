c145f6c359f7f488f77b5e41368ae6d18397e4af
akmt14 <akshay.kamath.14@gmail.com>
2022-10-30 15:22:43 +0530

    historic file load process change

--- src/historic/dataloadprep.py
+++ src/historic/dataloadprep.py
@@ -14,7 +14,11 @@ def transformer():
     for _file in os.listdir(path=path):
         if _file.endswith('.txt'):
             filename = _file.split('.')[0]+'.csv'
-            df = pd.read_fwf(path+_file, sep="\t", header=None, engine='python', encoding='utf-8')
+            data = []
+            with open(path+_file, 'r') as f:
+                for line in f:
+                    data.append(line.split())
+            df = pd.DataFrame(data)
             df.columns = columns
             df["state"] = os.path.basename(_file.split('.')[0].split("_")[0])
             df["city"] = os.path.basename(" ".join(i for i in (_file.split('.')[0].split("_")[1:])))
