98c306bb0cf9f0352bc3a60d5761632266750221
akmt14 <akshay.kamath.14@gmail.com>
2022-11-01 09:31:49 +0530

    load meteostat data

--- src/docker/dags/sql/batch_upload.sh
+++ src/docker/dags/sql/batch_upload.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-hist_fpath="./data/01_historic/local/data_load/"
+hist_fpath="./data/01_historic/local/data_load/universityofdayton/"
 
 for f in $hist_fpath*.csv;do
-    cat $f | psql -U postgres -d projects -c "COPY weather.daily_historic_local(month, day, year, temp_f, state, city) FROM STDIN DELIMITER ',' "
+    cat $f | psql -U postgres -d projects -c "COPY weather.daily_historic_local_ud(month, day, year, temp_f, state, city) FROM STDIN DELIMITER ',' "
+done
+
+hist_fpath="./data/01_historic/local/data_load/meteostat/"
+
+for f in $hist_fpath*.csv;do
+    cat $f | psql -U postgres -d projects -c "COPY weather.daily_historic_local_m(time, tavg, tmin, tmax, prcp, snow, wdir, wspd, wpgt, pres, tsun, lat, lon, add) FROM STDIN DELIMITER ',' CSV HEADER;"
 done
