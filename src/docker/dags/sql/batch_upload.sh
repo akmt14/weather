01d2dddb27b5186d2389118ea4a59150764e382d
akmt14 <akshay.kamath.14@gmail.com>
2022-11-05 19:40:13 +0530

    raw prefix added to source tbls

--- src/docker/dags/sql/batch_upload.sh
+++ src/docker/dags/sql/batch_upload.sh
@@ -3,11 +3,11 @@
 hist_fpath="./data/01_historic/local/data_load/universityofdayton/"
 
 for f in $hist_fpath*.csv;do
-    cat $f | psql -U postgres -d projects -c "COPY weather.daily_historic_local_ud(month, day, year, temp_f, state, city) FROM STDIN DELIMITER ',' "
+    cat $f | psql -U postgres -d projects -c "COPY weather.raw_daily_historic_local_ud(month, day, year, temp_f, state, city) FROM STDIN DELIMITER ',' "
 done
 
 hist_fpath="./data/01_historic/local/data_load/meteostat/"
 
 for f in $hist_fpath*.csv;do
-    cat $f | psql -U postgres -d projects -c "COPY weather.daily_historic_local_m(time, tavg, tmin, tmax, prcp, snow, wdir, wspd, wpgt, pres, tsun, lat, lon, add) FROM STDIN DELIMITER ',' CSV HEADER;"
+    cat $f | psql -U postgres -d projects -c "COPY weather.raw_daily_historic_local_m(time, tavg, tmin, tmax, prcp, snow, wdir, wspd, wpgt, pres, tsun, lat, lon, add) FROM STDIN DELIMITER ',' CSV HEADER;"
 done
