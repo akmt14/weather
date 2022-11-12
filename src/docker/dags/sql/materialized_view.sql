1b8b3422ef8a1cfb35a557b2cab6051142a1c871
akmt14 <akshay.kamath.14@gmail.com>
2022-11-12 06:29:59 +0530

    mat view create

---
+++ src/docker/dags/sql/materialized_view.sql
@@ -0,0 +1,26 @@
+CREATE MATERIALIZED VIEW weather.mv_temp AS (
+WITH weather_tbl AS (
+	SELECT fd.datetime AS Date,
+			fd.latitude AS Latitude,
+			fd.longitude AS Longitude,
+			fd.tempmax AS Max_Temperature,
+			fd.tempmin AS Min_Temperature,
+			ROUND(CAST((fd.tempmin + fd.tempmax)/2 AS numeric), 1) AS Avg_Temperature
+	FROM weather.f_daily fd
+	UNION
+	SELECT fm.datetime AS Date,
+			fm.latitude AS Latitude,
+			fm.longitude AS Longitude,
+			fm.tmax AS Max_Temperature,
+			fm.tmin AS Min_Temperature,
+			fm.tavg AS Avg_Temperature
+	FROM weather.f_hist fm
+)
+
+SELECT w.*, resolvedaddress 
+FROM weather_tbl w
+JOIN weather.dim_location dl 
+ON CAST(dl.latitude AS DOUBLE PRECISION) = w.latitude AND CAST(dl.longitude AS DOUBLE PRECISION) = w.longitude
+);
+
+
